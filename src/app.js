feb963558468299e511e5adda20e21c0b3511e65
Maeva <maevashali@gmail.com>
2021-08-22 08:21:44 +0200

    Updated the HTML document and the time

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
+function formatDate(timestamp) {
+  let date = newDate(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   let cityNameElement = document.querySelector("#city-name");
-  let dateElement = document.querySelector("#date");
+  let dateElement = document.querySelector("#currentDate");
   let temperatureElement = document.querySelector("#temperature");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   cityNameElement.innerHTML = response.data.name;
   temperatureElement = Math.round(response.data.main.temp);
-
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   description.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "0d800c588dc0c17f7124ec074768af05";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Antananarivo&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showTemperature);
